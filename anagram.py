6419cd4ec78590960fbcd8aa8449d6e51455a9c1
DanTeixeira <43186561+1337M0N5T3R@users.noreply.github.com>
2018-10-23 20:14:49 +0100

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -3,14 +3,17 @@ from itertools import permutations
 
 palavra = input('Escreve uma palavra até 10 letras: ').casefold()
 
-if len(palavra) <= 10:
+if palavra.isalpha() and len(palavra) <= 10 :
     lista=sorted(set(["".join(perm) for perm in permutations(palavra)]))
     print(random.choice(lista))
 
 elif len(palavra) > 10:
     print('Não inventes. MENOS de 10 letras sff.')
 
-while len(palavra) <= 10:
+else:
+    print('Não inventes. Usa LETRAS sff.')
+
+while palavra.isalpha() and len(palavra) <= 10:
     decisao = input("Queres outro anagrama com esta palavra? (sim/nao)").casefold()
 
     if decisao == "sim" or decisao == "s" or decisao == "y":
