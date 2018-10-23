7b67f3b722a32326c1e3a6b8e68cc1acfd999127
DanTeixeira <43186561+1337M0N5T3R@users.noreply.github.com>
2018-10-23 16:24:41 +0100

    Add files via upload

--- anagram.py
+++ anagram.py
@@ -1,11 +1,1 @@
-import random
-from itertools import permutations
-
-palavra = input('Escreve uma palavra até 10 letras: ')
-
-if len(palavra) <= 10:
-    lista=sorted(set(["".join(perm) for perm in permutations(palavra)]))
-    print(random.choice(lista))
-
-elif len(palavra) > 10:
-    print('MemoryError')
+print('MemoryError')
